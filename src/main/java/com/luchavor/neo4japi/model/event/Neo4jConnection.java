7aaedcd366169ed063aeae8faa905627a7ac33f5
chemch <cac692@gmail.com>
2023-07-01 14:59:21 -0500

    zz

--- src/main/java/com/luchavor/neo4japi/model/event/Neo4jConnection.java
+++ src/main/java/com/luchavor/neo4japi/model/event/Neo4jConnection.java
@@ -1,17 +1,15 @@
-//package com.luchavor.neo4japi.model.event;
-//
-//import java.util.UUID;
-//
-//import org.springframework.data.neo4j.core.schema.GeneratedValue;
-//import org.springframework.data.neo4j.core.schema.Id;
-//import org.springframework.data.neo4j.core.schema.Node;
-//
-//import com.luchavor.datamodel.event.Connection;
-//
-//import lombok.Data;
-//
-//@Node("Connection")
-//public class Neo4jConnection extends Connection {
-//	// neo4j id
-//	@Id @GeneratedValue private UUID id;
-//}
+package com.luchavor.neo4japi.model.event;
+
+import java.util.UUID;
+
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+
+import com.luchavor.datamodel.event.Connection;
+
+@Node("Connection")
+public class Neo4jConnection extends Connection {
+	// neo4j id
+	@Id @GeneratedValue private UUID id;
+}
