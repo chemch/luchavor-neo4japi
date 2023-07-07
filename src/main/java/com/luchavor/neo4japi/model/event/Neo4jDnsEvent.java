692d0a23b43e4102ce9c312259453e6019842126
chemch <cac692@gmail.com>
2023-07-06 22:09:03 -0500

    pp

--- src/main/java/com/luchavor/neo4japi/model/event/Neo4jDnsEvent.java
+++ src/main/java/com/luchavor/neo4japi/model/event/Neo4jDnsEvent.java
@@ -5,10 +5,10 @@ import java.util.UUID;
 import org.springframework.data.neo4j.core.schema.GeneratedValue;
 import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
-import com.luchavor.datamodel.event.dns.DnsEvent;
+import com.luchavor.datamodel.event.dns.DnsEventImpl;
 
 @Node("DnsEvent")
-public class Neo4jDnsEvent extends DnsEvent {
+public class Neo4jDnsEvent extends DnsEventImpl {
 	// neo4j id
 	@Id @GeneratedValue private UUID id;
 }
