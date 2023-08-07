70a6fd12318fe12fbe1b30923300b6a0c281fac5
chemch <cac692@gmail.com>
2023-08-06 20:33:45 -0500

    rr

--- src/main/java/com/luchavor/neo4japi/persistence/detection/state/ClosedDetectionStateRepo.java
+++ src/main/java/com/luchavor/neo4japi/persistence/detection/state/ClosedDetectionStateRepo.java
@@ -1,13 +1,13 @@
-package com.luchavor.neo4japi.persistence.detection.state;
-
-import java.util.UUID;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-import com.luchavor.datamodel.detection.state.ClosedDetectionState;
-
-@RepositoryRestResource(collectionResourceRel = "closedDetectionState", path = "closedDetectionState")
-public interface ClosedDetectionStateRepo extends PagingAndSortingRepository<ClosedDetectionState, UUID>, CrudRepository<ClosedDetectionState, UUID> {
-}
+//package com.luchavor.neo4japi.persistence.detection.state;
+//
+//import java.util.UUID;
+//
+//import org.springframework.data.repository.CrudRepository;
+//import org.springframework.data.repository.PagingAndSortingRepository;
+//import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+//
+//import com.luchavor.datamodel.detection.state.ClosedDetectionState;
+//
+//@RepositoryRestResource(collectionResourceRel = "closedDetectionState", path = "closedDetectionState")
+//public interface ClosedDetectionStateRepo extends PagingAndSortingRepository<ClosedDetectionState, UUID>, CrudRepository<ClosedDetectionState, UUID> {
+//}
--- src/test/java/com/luchavor/neo4japi/persistence/detection/DetectionPersistenceTests.java
+++ src/test/java/com/luchavor/neo4japi/persistence/detection/DetectionPersistenceTests.java
@@ -1,51 +1,51 @@
-package com.luchavor.neo4japi.persistence.detection;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import com.luchavor.datamodel.artifact.network.session.ssl.SslEvent;
-import com.luchavor.datamodel.detection.Detection;
-import com.luchavor.datamodel.detection.DetectionImpl;
-import com.luchavor.datamodel.detection.DetectionTests;
-import com.luchavor.neo4japi.persistence.detection.state.ClosedDetectionStateRepo;
-import com.luchavor.neo4japi.persistence.detection.state.OpenDetectionStateRepo;
-
-@SpringBootTest
-@ActiveProfiles("test")
-public class DetectionPersistenceTests {
-	private DetectionTests detectionTests = new DetectionTests();
-	
-	@Autowired
-	DetectionRepo detectionRepo;
-	
-	@Autowired
-	ClosedDetectionStateRepo closedDetectionStateRepo;
-	
-	@Autowired
-	OpenDetectionStateRepo openDetectionStateRepo;
-	
-    @BeforeEach
-    void deleteAllBeforeTests() throws Exception {
-    	detectionRepo.deleteAll();
-    	openDetectionStateRepo.deleteAll();
-    	closedDetectionStateRepo.deleteAll();
-	}
-    
-    @Test
-    void shouldAddDetectionsSafely() throws Exception {
-    	// load all detection types
-		loadDetections();
-	}
-    
-    private void loadDetections() throws Exception {
-    	Detection<?, ?, ?> detection1 = detectionTests.getDetection1();
-		// examine object for nullness
-		assertNotNull(detection1);
-		// save objects
-		detectionRepo.save((DetectionImpl<?, ?, ?>) detection1);
-    }
-}
+//package com.luchavor.neo4japi.persistence.detection;
+//
+//import static org.junit.jupiter.api.Assertions.assertNotNull;
+//
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.ActiveProfiles;
+//import com.luchavor.datamodel.artifact.network.session.ssl.SslEvent;
+//import com.luchavor.datamodel.detection.Detection;
+//import com.luchavor.datamodel.detection.DetectionImpl;
+//import com.luchavor.datamodel.detection.DetectionTests;
+//import com.luchavor.neo4japi.persistence.detection.state.ClosedDetectionStateRepo;
+//import com.luchavor.neo4japi.persistence.detection.state.OpenDetectionStateRepo;
+//
+//@SpringBootTest
+//@ActiveProfiles("test")
+//public class DetectionPersistenceTests {
+//	private DetectionTests detectionTests = new DetectionTests();
+//	
+//	@Autowired
+//	DetectionRepo detectionRepo;
+//	
+//	@Autowired
+//	ClosedDetectionStateRepo closedDetectionStateRepo;
+//	
+//	@Autowired
+//	OpenDetectionStateRepo openDetectionStateRepo;
+//	
+//    @BeforeEach
+//    void deleteAllBeforeTests() throws Exception {
+//    	detectionRepo.deleteAll();
+//    	openDetectionStateRepo.deleteAll();
+//    	closedDetectionStateRepo.deleteAll();
+//	}
+//    
+//    @Test
+//    void shouldAddDetectionsSafely() throws Exception {
+//    	// load all detection types
+//		loadDetections();
+//	}
+//    
+//    private void loadDetections() throws Exception {
+//    	Detection<?, ?, ?> detection1 = detectionTests.getDetection1();
+//		// examine object for nullness
+//		assertNotNull(detection1);
+//		// save objects
+//		detectionRepo.save((DetectionImpl<?, ?, ?>) detection1);
+//    }
+//}
