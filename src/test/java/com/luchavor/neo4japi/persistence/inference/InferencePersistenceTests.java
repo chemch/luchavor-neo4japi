ef8698a4699a6db042862dbfbc9a1412152ca916
chemch <cac692@gmail.com>
2023-08-08 16:42:32 -0500

    zz

--- src/test/java/com/luchavor/neo4japi/persistence/inference/InferencePersistenceTests.java
+++ src/test/java/com/luchavor/neo4japi/persistence/inference/InferencePersistenceTests.java
@@ -5,19 +5,12 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import com.luchavor.datamodel.artifact.Artifact;
-import com.luchavor.datamodel.artifact.ArtifactImpl;
-import com.luchavor.datamodel.artifact.ArtifactTests;
-import com.luchavor.datamodel.artifact.network.session.Session;
 import com.luchavor.datamodel.inference.Inference;
 import com.luchavor.datamodel.inference.InferenceImpl;
 import com.luchavor.datamodel.inference.InferenceTests;
 import com.luchavor.datamodel.inference.inferredHost.InferredHost;
-import com.luchavor.neo4japi.persistence.artifact.network.session.state.ClosedSessionStateRepo;
-import com.luchavor.neo4japi.persistence.artifact.network.session.state.OpenSessionStateRepo;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -34,11 +27,10 @@ public class InferencePersistenceTests {
 
     @Test
     void loadSessionArtifacts() throws Exception {
-    	Inference<?> rv =  tests.getInference1();
-		// examine object for nullness
+    	Inference<InferredHost> rv =  tests.getInference1();
+    	// check for nullness
 		assertNotNull(rv);
 		// save objects
 		repo.save((InferenceImpl<?>) rv);
-		
     }
 }
