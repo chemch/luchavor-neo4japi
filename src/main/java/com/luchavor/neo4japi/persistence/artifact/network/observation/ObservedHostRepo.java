0b01a17f89f4c948232c4aa176c5b61fbff53f2d
chemch <cac692@gmail.com>
2023-08-24 22:49:39 -0500

    vv

--- src/main/java/com/luchavor/neo4japi/persistence/artifact/network/observation/ObservedHostRepo.java
+++ src/main/java/com/luchavor/neo4japi/persistence/artifact/network/observation/ObservedHostRepo.java
@@ -9,5 +9,5 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import com.luchavor.datamodel.artifact.network.observation.observedhost.ObservedHostImpl;
 
 @RepositoryRestResource(collectionResourceRel = "host", path = "host")
-public interface HostRepo extends PagingAndSortingRepository<ObservedHostImpl, UUID>, CrudRepository<ObservedHostImpl, UUID> {
+public interface ObservedHostRepo extends PagingAndSortingRepository<ObservedHostImpl, UUID>, CrudRepository<ObservedHostImpl, UUID> {
 }
