c1e43b5fa620b631c7e09569818772bf37e6b86c
chemch <cac692@gmail.com>
2023-10-14 14:57:40 -0400

    bb

--- src/main/java/com/luchavor/neo4japi/persistence/artifact/network/observation/ObservedHostRepo.java
+++ src/main/java/com/luchavor/neo4japi/persistence/artifact/network/observation/ObservedHostRepo.java
@@ -4,14 +4,9 @@ import java.util.UUID;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import com.luchavor.datamodel.artifact.network.observation.observedhost.ObservedHost;
 import com.luchavor.datamodel.artifact.network.observation.observedhost.ObservedHostImpl;
-import com.luchavor.datamodel.artifact.network.session.connection.Connection;
-import com.luchavor.datamodel.artifact.network.session.event.SessionEventType;
-import java.util.List;
 import java.util.Optional;
 
 
