b6f19fcd9cf6c48942deb951c4c6fdee8d3d6f3a
chemch <cac692@gmail.com>
2024-02-19 16:49:09 -0500

    rr

--- src/main/java/com/luchavor/neo4japi/dao/ArtifactDao.java
+++ src/main/java/com/luchavor/neo4japi/dao/ArtifactDao.java
@@ -1,17 +1,11 @@
 package com.luchavor.neo4japi.dao;
 
-import java.util.Optional;
-import java.util.UUID;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.luchavor.datamodel.artifact.Artifact;
 import com.luchavor.datamodel.artifact.ArtifactImpl;
-import com.luchavor.datamodel.artifact.network.observation.observedhost.ObservedHost;
-import com.luchavor.datamodel.artifact.network.observation.observedhost.ObservedHostImpl;
 import com.luchavor.neo4japi.persistence.artifact.ArtifactRepo;
-import com.luchavor.neo4japi.persistence.artifact.network.observation.ObservedHostRepo;
 
 @Component
 public class ArtifactDao {
